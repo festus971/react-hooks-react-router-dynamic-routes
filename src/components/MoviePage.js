e029bc138cf457dde84be5cf8724e59de50a307c
+ <+>
2022-07-20 17:51:59 +0300

    done

---
+++ src/components/MoviePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, useRouteMatch } from "react-router-dom";
+import MovieList from "./MovieList";
+import MovieShow from "./MoviesShow";
+
+function MoviesPage({ movies }) {
+  const match=useRouteMatch();
+  return (
+    <div>
+      <MovieList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>CHoose a movie from the list above</h3></Route>
+        <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies}/>
+        </Route>
+    </div>
+  );
+}
+export default MoviesPage;
